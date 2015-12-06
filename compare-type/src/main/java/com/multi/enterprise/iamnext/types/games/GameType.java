b1dcec331ef828d8fe4c2c99fa5593b8a9850d92
Rohit Dhumal <Rohit@rohits-air.home>
2015-12-06 17:46:41 -0500

    Setting base

--- compare-type/src/main/java/com/multi/enterprise/iamnext/types/games/GameType.java
+++
@@ -1,5 +0,0 @@
-package com.multi.enterprise.iamnext.types.games;
-
-public enum GameType {
- POOL,DART,JENGA
-}
--- compare-type/src/main/java/com/multi/enterprise/iamnext/types/location/LocationType.java
+++
@@ -1,5 +0,0 @@
-package com.multi.enterprise.iamnext.types.location;
-
-public enum LocationType {
- BAR,RESTAURANT, BAR_AND_RESTAURANT, LOUNGE
-}
